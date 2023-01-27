4aec901efcabd205f051bc243b96f4b43c135cc5
cssmith07060 <cssmith07060@gmail.com>
2023-01-27 14:46:49 -0500

    commit 3

--- block.js
+++ block.js
@@ -1,12 +1,12 @@
 class Block {
     constructor({timestamp, lastHash, hash, data}) {
-        this.timestamp = timestamp; 
-        this.lastHash = lastHash;
-        this.hash = hash;
-        this.data = data;
+        // this.timestamp = timestamp; 
+        // this.lastHash = lastHash;
+        // this.hash = hash;
+        // this.data = data;
     }
 }
 
 const block1 = new Block({ timestamp: '01/01/01', lastHash: 'foo-lastHash', hash: 'foo-hash', data: 'foo-data' });
 
-module.exports = blocks
+module.exports = block;
